27fc7b64bf3603bd80d358fc8e20303c555ab628
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-17 17:07:11 -0600

    pushed persona content and interactive flow content

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
 <!DOCTYPE html>
-<html lang="utr-8">
+<html lang="utf-8">
 		<head>
 			<title>Data Design Project</title>
 		</head>
 		<body>
-			<header>Persona</header>
+			<header>Data Design Project</header>
 			<main>
 				<h1>Data Design Project</h1>
-				<h2>Persona</h2>
-				<p>| Giles Sandoval | 30 years old</p>
-				
-				<p>a;woeif</p>
+
+				<!-- Persona section -->
+				<h2>Persona | Giles Sandoval | 30 years old</h2>
+				<p>Profession: Youth minister. Leads a group of youths between the ages of 18-29 years old. Performs weekly messages to youth during weekdays. Does his bible study in the mornings and once in the evening. Requires a lexicon in the Greek and Hebrew definitions of the bible. Compared to the English language, A single word in the Greek or Hebrew could have multiple meanings in the English. He needs a lexicon to translate a word to word translation. He reads from the New King James Version of the bible, which is a literal (“word-to-word”) translation. And the he uses the Message version or the New Living Translation version as a “thought-to-thought” translation for the bible. He needs a Hebrew/Greek lexicon dictionary to help him dive deeper into the word.</p>
+				<p>Technology: Windows 10 user on his laptop, Android phone, Samsung Galaxy 5s with up to date version on his phone, Uses his Samsung smart 60” TV to link his phone or his laptop to help him have multiple screens or watch videos. Occasionally uses the HDMI cable to hook to his TV.</p>
+				<p>Attitudes and behavior: He works in a small church with many events throughout the week that he is encouraged to attend. He spends anywhere from 8-12 hours a week preparing a message for the youth ministry. He is a mentor, counselor, and a leader for his youth that approach him for fellowship or to help him with problems. He gets frustrated with the lag of a site, if he’s browsing. Will wait for it to load, but dislikes it extremely when he has to wait. He prefers an online lexicon/dictionary over an actual huge dictionary he has to carry around to help him study. He’d rather have it on his phone and be done with it. If a site lets him down. His alternative will be to go to his concordance dictionary that is through his kindle app on his phone. He is a huge social media user, since his youth does the same. He has accounts he frequents on Twitter, Instagram, Snapchat and facebook, so he is accessible to his ministry via social media. He is also on Linkedin, mainly to connect with other pastors in the church and other churches internationally. He also takes part in 2 mission trips per year, for about a week at a time. This enables him to access his tools via internet/phone/laptop.</p>
+				<p>Frustrations and needs: He is human. He has a point where he knows if he’s losing it, He has to unplug from the world and pray and read the word, and talk with God. He is in ministry so he gets paid a minimum salary. He has a goal to write a book to hopefully supplement money for the church and his future as a youth minister and future pastor in the church. He uses his head pastor as a mentor to help him through certain situations and his pastor uses a concordance/lexicon for his bible studies. With all these responsibilities and his natural spirit to want to help others, it’s easy to feel he’s pulled in different directions. He needs his quiet time with God in the early mornings, in the evenings, and the occasional emergency times during the day.</p>
+				<p>Goals: His main goal is that when he speaks the youth understands where he’s coming from when explaining subjects, concepts, and definitions about the bible. His goal is to keep the message as simple as possible. He wants the youth ministry to apply it to their lives in a practical manner through painting a picture. He would like to keep his life as simple as he could so that when a student/friend/or Christ follower asks what he does or how he does it…He has specifics to give them. But the freedom to tailor what he does.</p>
+
+				<!-- Use case section -->
+				<h2>Use Case</h2>
+				<p>His lead pastor gave him a task to speak on finances for the weekly message coming up. The lead pastor feels that it’s a subject that should be addressed to the youth ministry. He wants him to create a message tailored around the original Greek definitions of 2 specific verses. Over 50% of Jesus ministry were financial principles. The pastor gave him 2 verses to dive into and create a message surrounding those 2 verses. He’s welcome to refer to other verses with that SAME word used in the Greek, to help support the 2 main verses. He will use the dictionary to define specific words in the verse to paint a picture of that specific word used in that context, in that time, in that culture, and the meaning of what the word meant at the time it was written. I don’t want to have to sign in to look up a word.</p>
+
+				<!-- Interaction Flow -->
+				<h2>Interaction Flow</h2>
+					<ul>
+						<li>“I want to enter a verse in the search field.”</li>
+						<li>“I want to see the verse in the results field and read it.”</li>
+						<li>“I want to hit a tab that brings me each word.”</li>
+						<li>“I want to see the verse in English and in the original Greek right next to it.”</li>
+						<li>“I want to click the link of the Greek word to lead me to the original Greek definition.”</li>
+						<li>“I want a button to let me know how it is pronounced.”</li>
+						<li>“I want a button that gives me information about the tense, voice, and mood of the word.”</li>
+						<li>“I’d also like to know how many times the word is used in the entire bible.”</li>
+						<li>“I would like a help button to let help me in case I have questions on what certain buttons or links do.”</li>
+						<li>“I would like a button to arrange how the sentence is said in English and how the sentence is arranged in Greek form.”</li>
+						<li>“I would like a reference to what version of the bible I’m reading in English and what scroll was used to translate that version in the original scroll of Greek.”</li>
+						<li>“I would like to be able to click a word and see how many times that word was used in that version in the bible.” </li>
+						<li>“I want to click on one of the words that gives me all the multiple meanings of that word in the Greek.” </li>
+						<li>“I want to be able to read multiple English versions parallel to each other when I’m studying.”</li>
+					</ul>
+
 			</main>
 		</body>
 		<footer>
