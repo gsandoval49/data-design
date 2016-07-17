94c90f5ce775c4864464c7ef957c598699d9392c
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-17 16:24:19 -0600

    added index for data design project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="utr-8">
+		<head>
+			<title>Data Design Project</title>
+		</head>
+		<body>
+			<header>Persona</header>
+			<main>
+				<h1>Data Design Project</h1>
+				<h2>Persona</h2>
+				<p>| Giles Sandoval | 30 years old</p>
+				
+				<p>a;woeif</p>
+			</main>
+		</body>
+		<footer>
+
+		</footer>
+</html>
