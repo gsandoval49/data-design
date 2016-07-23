f3d128efe14d4f73b6206c8088fcb204f1e4cbc4
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-23 12:49:27 -0600

    created table for prfile in sql

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -6,12 +6,15 @@ DROP TABLE IF EXISTS verseFavorite;
 DROP TABLE IF EXISTS verse;
 DROP TABLE IF EXISTS profile;
 
--- the CREATE TABLE function is a function that takes tons of arguments to layout the table's schema
--- this command creates a table
+-- this is a comment in SQL (yes, the space is needed!)
+-- these statements will drop the tables and re-add them
+-- CREATE TABLE command creates a table
 -- INT UNSIGNED means positive interger; AUTO INCREMENT means auto populates a # for profileId; and
 -- NOT NULL means it's required field - NULL in the latin means none or zero (0). To make something optional
 -- take out the NOT NULL command and leave it blank.
 -- Keep in mind to begin and end with the PRIMARY KEY = profileId (in this case)
+
+-- the CREATE TABLE function is a function that takes tons of arguments to layout the table's schema
 CREATE TABLE profile (
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
@@ -22,3 +25,4 @@ CREATE TABLE profile (
 	-- this officiates the primary key for the entity
 	PRIMARY KEY (profileId)
 );
+
