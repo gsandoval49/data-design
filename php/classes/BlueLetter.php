31392452f1daefb171abee1ea79552c856adbe57
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-24 22:31:29 -0600

    added accessor and method mutators for profile id and profile email.

--- php/classes/BlueLetter.php
+++ php/classes/BlueLetter.php
@@ -12,16 +12,68 @@
  **/
 class Profile {
 	/**
-	 * id for the Profile; this is a primary key
-	 */
+	 * id for the Profile; this is a primary key.
+	 **/
 	private $profileId;
 	/**
 	 * email for the user of this profile.
-	 */
+	 **/
 	private $profileEmail;
 	/**
+	 * first name of this person.
+	 **/
+	private $profileFirstName;
+	/**
+	 * last name of this person.
+	 **/
+	private $profileLastName;
+
+	/**
+	 * accessor method for profile id
 	 *
-	 */
+	 * @return int value of profile id
+	 **/
+	public function getProfileId() {
+		return($this->profileId);
+	}
+
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param int $newProfileId new value for user
+	 * @throws UnexpectedValueException if $newProfileId is not an integer
+	 **/
+	public function setProfileId($newProfileId) {
+		// verify the profile id is valid
+		$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+		if($newProfileId === false) {
+			throw(new UnexpectedValueException("profile id is not an interger"));
+		}
+
+		// convert and store the profile id
+		$this->profileId = intval($newProfileId);
+	}
+
+	/**
+	 * accessor method for profile email
+	 *
+	 * @return string value of email
+	 **/
+	public function getProfileEmail() {
+		return($this->profileEmail);
+	}
+
+	/**
+	 * mutator method for profile email
+	 *
+	 * @param string $newProfileEmail new value of the email
+	 * @throws UnexpectedValueException if $newProfileEmail is not valid
+	 **/
+	public function setProfileEmail($newProfileEmail) {
+		// verify that the email is valid
+		//check to see if there is a check for email specifically for validity.
+	}
+
 }
 
 ?>
