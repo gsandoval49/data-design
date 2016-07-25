dc475e52a148f398b212c0bcfad2e81443656bb2
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-24 21:18:31 -0600

    added blue letter php file; added introduction to first methods

---
+++ php/classes/BlueLetter.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Profile for blue letter bible site
+ *
+ * This profile is data collected about user for login and user benefit purposes.
+ * This can be extended to include information such as name.
+ *
+ * @author Giles Sandoval <gsandoval49@cnm.edu>
+ **/
